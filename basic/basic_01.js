91c6e4c6f495311fbbf61c7809b12afe082f1952
Juhisingh11500 <juhi11500@gmail.com>
2025-12-18 04:23:10 +0000

    variables

---
+++ basic/basic_01.js
@@ -0,0 +1,26 @@
+console.log("juhi");
+//in compailar if const, let, var get it means it is assign im memory.
+
+const userEmail = "juhi@gmail.com"
+
+let userName = "juhi_singh"
+
+var userId = 12234
+
+accountCity = "bihar"
+
+// userEmail = "tiku@gmail.com"// not allowed
+console.log(userEmail);
+userName = "tiku"
+userId = 2222
+accountCity = "nagpur"
+let accountState;
+console.table([userEmail,userName,userId,accountCity,accountState])
+
+
+{}//scope
+
+/*
+prefer not to use var
+because of issue in block scope and funtional scope
+*/
