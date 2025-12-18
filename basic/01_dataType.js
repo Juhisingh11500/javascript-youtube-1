f9ee9a10c73cd158295ccc552a25cc558f686b39
Juhisingh11500 <juhi11500@gmail.com>
2025-12-18 13:30:20 +0000

    datatypes

---
+++ basic/01_dataType.js
@@ -0,0 +1,43 @@
+//DataTypes and ECMA Standards
+
+//all codes are treated as new code stander
+
+"use strict";//treat all js code as newer version.
+
+//alert(3 + 3)//ReferenceError: alert is not defined in node.js.it is define in browser.
+
+// console.log(3 + 3) console.log("juhi")//SyntaxError: Unexpected identifier 'console'
+
+//console.log(3 + 3); console.log("juhi");//code readability should be high.
+
+/*console.log(3 + 3);
+console.log("juhi");*/
+
+//DataTypes
+
+let name ="juhi"
+let age = 26
+let isLoggedIn = true
+let state = null
+let fruit;//undefined
+
+//premative dataTypes
+
+//1. number => 2 to the power 53
+//2. bigint
+//3. string => ""
+//4. boolean => true/false
+//5. null => standalone value
+//6. undefined => 
+//7. symbol => //uniqueness define 
+
+//unpremative dataTypes
+
+//object
+
+/* console.log(typeof "JUHI");
+console.log(typeof null);//object
+console.log(typeof age);
+console.log(typeof undefined);//undefined */
+
+
