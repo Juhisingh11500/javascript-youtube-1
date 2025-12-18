66ad31f1274470a04ebf1cb8d313e8700f64ae82
Juhisingh11500 <juhi11500@gmail.com>
2025-12-18 15:45:54 +0000

    operations

---
+++ basic/03_dataType.js
@@ -0,0 +1,43 @@
+//string to number conversion.
+
+//******************** operations***************
+
+let value = 4
+let negValue = -value
+//console.log(negValue);
+
+
+/*console.log(2 + 2);
+console.log(2 - 2);
+console.log(2 * 2);
+console.log(2 % 2);
+console.log(2 ** 2);
+console.log(2 / 2); */
+
+/* let str1 = "hello"
+let str2 = " juhi"
+let str3 = str1 + str2
+console.log(str3); */
+
+/* console.log("2" + 1);//21
+console.log(2 + "1");//21
+console.log("2" + "2");//22
+console.log("2" + "2" + 2);//222
+console.log("2" + 2 + "2");//222
+console.log("2" + 2 + 2);//222
+console.log(2 + 2 + "2");//42 */ //it is very messy code so not in used
+
+//console.log( (3 + 7) * 8 % 2); //good practice which is solve first
+
+//console.log(+true); //1
+//console.log(true+); //SyntaxError: Unexpected token ')'
+//console.log(+""); //0
+
+/* let num1, num2, num3
+num1 = num2 = num3 = 3 + 4 */ //not good practice
+
+let gameCounter = 100
+gameCounter++;//postfix 101
+++gameCounter;//prefix  101
+
+console.log(gameCounter);//101
