cc5db01bca5eefbe8702cb2e8dab6f2dbbe994cd
Juhisingh11500 <juhi11500@gmail.com>
2025-12-19 03:57:21 +0000

    comparision ofdataTypes

---
+++ basic/4_dataType.js
@@ -0,0 +1,41 @@
+// comparision of data types
+
+// console.log( 2 > 1);
+// console.log( 2 >= 1);// true
+// console.log( 2 < 1);
+// console.log( 2 <= 1);
+// console.log( 2 == 1);
+// console.log( 2 != 1);
+
+// console.log( "2" > 1);// true
+// console.log( "02" > 1); // true
+
+//before compare the data check the dataTypes
+
+/* console.log(null > 0); //F
+console.log(null == 0);//F
+console.log(null >= 0);//T
+console.log(null < 0);//F
+console.log(null <= 0);//T */
+
+/*
+Comparison operators (< > <= >=) use karte waqt JavaScript:
+👉 null ko number me convert kar deta hai
+ == aur === me null sirf null ke barabar hota hai
+null == undefined ✔️
+kisi number ke barabar ❌
+*/
+
+/* console.log(undefined == 0);//F
+console.log(undefined === 0);//F
+console.log(undefined > 0);//F
+console.log(undefined >= 0);//F
+console.log(undefined <= 0);//F
+console.log(undefined < 0);//F
+console.log(undefined == null);//T */ //avoid this type conversion
+
+//=== value + dataType strict type data check
+
+// console.log("9" == 9); //T
+// console.log("9" === 9); //F
+
