39b882f790cd20586c03c1822afd973a6a60749a
Juhisingh11500 <juhi11500@gmail.com>
2025-12-22 13:23:55 +0000

    object_destructure and  json

---
+++ basic_2/3_object.js
@@ -0,0 +1,36 @@
+//destructuring of object
+
+let user = {
+    fulllname: "juhisingh",
+    age: 25,
+    location:"bihar",
+    email: "juhi11500@gmail.com"
+}
+
+//console.log(user.fulllname);
+//console.log(user["fulllname"]);
+//destucturing
+
+// let {fulllname} = user
+// console.log(fulllname);
+
+// let {fulllname:name} = user
+// console.log(name);
+
+//*************api concept******************
+
+//pahle api ka value backend se xml structure me aati which is very complicated
+//now values come in mostly json form
+
+// {
+//     "name": "juhi",
+//     "age" : 23,
+//     "location": "bihar"
+//}
+
+// [
+//     {},
+//     {},
+//     {}
+// ]
+
