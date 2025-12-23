8f8b9796a23066813de199877f6638f8fb837ed1
Juhisingh11500 <juhi11500@gmail.com>
2025-12-23 04:04:24 +0000

    scope level

--- basic_2/scope.js
+++ basic_2/scope.js
@@ -18,7 +18,7 @@ if (true) {
     let a =5
     var b = 7
     const c = 6
-    console.log("inner", a);//5
+   // console.log("inner", a);//5
     
 }
 //console.log(a);//ReferenceError: a is not defined
@@ -28,7 +28,52 @@ if (true) {
 //console.log(a);//8
 
 for (let i = 0; i < 6; i++) {
-   console.log(i);//0 1 2 3 4 5
+   //console.log(i);//0 1 2 3 4 5
 }
 //console.log(i);//ReferenceError: i is not defined
 //browser me scope alag hai aur code enviroment me scope alag h
+
+//*********** scope level and mini hosting****************
+
+function one() {
+    let username = "juhi"
+
+    function two() {
+        let website ="youtube"
+        //console.log(username);//juhi
+    }
+    //console.log(website);//ReferenceError: website is not defined
+    two()
+}
+one()
+
+if (true) {
+    let username = "tiku"
+    if (true) {
+        let age = 1
+        //console.log(`name is ${username} age is ${age} year`);
+    }
+    //console.log(age);//ReferenceError: age is not defined
+} 
+    //console.log(username);//ReferenceError: username is not defined
+    
+//*********** interesting **************
+let total = addone(7)
+function addone(num) {
+    return num + 1
+}
+console.log(total);//8
+
+//let total = addone(7)
+//console.log(total);//8
+
+console.log(addtwo(5));//ReferenceError: Cannot access 'addtwo' before initialization
+let addtwo = function (num) {
+    //when we store a funtion in a variable then it is called expretion
+    return num + 2
+}
+
+//addtwo(5)
+//console.log(addtwo(5));//7
+
+//function ke declaration pe depend karta h ki hum usse acess kar pate h ya nhi before initilisation 
