9230a87fb587a36473f97a24e472298916bb4cc8
Juhisingh11500 <juhi11500@gmail.com>
2025-12-24 16:45:57 +0000

    not complite

---
+++ control_flow/2_loop.js
@@ -0,0 +1,7 @@
+//for loop/itterators with break and continue
+
+for (let i = 0; i < 9; i++) {
+    const element = i;
+    //console.log(element);
+}
+//console.log(i);//ReferenceError: i is not defined
