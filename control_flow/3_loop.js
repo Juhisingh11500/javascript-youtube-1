a3e42bdf79500c4e8e54858ff120c67a1a55055b
Juhisingh11500 <juhi11500@gmail.com>
2025-12-26 05:52:48 +0000

    dowhile loop and while loop

---
+++ control_flow/3_loop.js
@@ -0,0 +1,32 @@
+//while and do while loop
+
+// let index = 0
+// while (index <= 10) {
+//     console.log(`value of index is ${index}`);
+//     //index++
+//     index = index + 2
+// }
+
+// let myarr = [1,2,5,8,9]
+// let index = 0
+// while (index < myarr.length) {
+//     //console.log(myarr[index]);
+//     index++
+// }
+//Jab iterations ka count pehle se sure na ho.
+//⚠️ Condition galat ho to infinite loop ho sakta hai.
+
+
+// let score = 1
+// do {
+//     console.log(`score is ${score}`);
+//     score++
+// } while (score <= 10);
+
+let score = 11
+do {
+    console.log(`score is ${score}`);//score is 11
+    score++
+} while (score <= 10);
+
+//Code kam se kam ek baar to chalega hi.
